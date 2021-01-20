d52d894f2b756112ffea061ff23e459b853c472f
Jose Horacio Dias De Oliveira <jhdoliveira@sp.gov.br>
2021-01-20 11:04:48 -0300

    .

---
+++ RESTCommon/ContadorRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTService
+{
+    public class ContadorRequest
+    {
+        public string Name { get; set; }
+    }
+}
